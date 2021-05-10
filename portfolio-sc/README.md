5edee1b226a41aa7baf1aa2795c251775a21bfda
sourabhsc <chauhans.sourabh@gmail.com>
2021-05-10 11:58:03 +0200

    My minimal portfolio website

---
+++ portfolio-sc/README.md
@@ -0,0 +1,26 @@
+# www.sourabhsc.wordpress.com
+
+----
+
+This is my personal website. 
+
+# Screenshots
+<p float="left">
+  <img src="https://raw.githubusercontent.com/crearo/portfolio/master/screenshots/home.png" width="400">
+</p>
+
+# Technology
+
+Developed using Python Flask, and a lot of CSS and Jinja2.
+Currently hosted on Heroku. 
+
+# I want to use this!
+
+You're free to use this for your own personal portfolio; but please do write to me telling me you have!
+ 
+ - change the static content wherever it is to suit your work.
+ - **NOTE**: Hard-refresh your browser (ctrl-shift-r) when you update the js files #19
+ - change the `experiences.json` and `projects.json` files with your work and projects. 
+ - send an email to rishextra at gmail dot com. 
+
+[The websie is inspired by this webpage](http://rish.space)
---
+++ portfolio-sc/static/js/home.js
@@ -0,0 +1,28 @@
+$(function() {
+  /* NOTE: hard-refresh the browser once you've updated this */
+  $(".typed").typed({
+    strings: [
+      "sourabh.AI -h <br/>" + 
+      "><span class='caret'>$</span> skills: Python, Machine learning, Flask,  computer vision<br/> ^100" +
+      /*"><span class='caret'>$</span> job: android auto at <a href='http://www.google.com/'>Google Munich</a><br/> ^100" +*/
+      "><span class='caret'>$</span> hobbies: Badminton, Travel, <a href='http://www.rish.space/blog'>writing</a><br/> ^300" +
+      "><span class='caret'>$</span> alias: sourabhsc <br/>" +
+      "><span class='caret'>$</span> highlight:  <a href='/projects/NULIRG'>Image segmentation on HST ULIRGs</a>, <a href='/projects/dharavicovid'>A JHU covid challenge solution</a><br/>"/*
+      "><span class='caret'>$</span> <a href='/timeline'>timeline</a> <a href='http://www.github.com/crearo/'>github</a> <a href='http://in.linkedin.com/in/bhardwajrish/'>linkedin</a> <a href='http://bhardwajrish.blogspot.com/'>blog</a><br/>"*/
+    ],
+    showCursor: true,
+    cursorChar: '_',
+    autoInsertCss: true,
+    typeSpeed: 0.001,
+    startDelay: 50,
+    loop: false,
+    showCursor: false,
+    onStart: $('.message form').hide(),
+    onStop: $('.message form').show(),
+    onTypingResumed: $('.message form').hide(),
+    onTypingPaused: $('.message form').show(),
+    onComplete: $('.message form').show(),
+    onStringTyped: function(pos, self) {$('.message form').show();},
+  });
+  $('.message form').hide()
+});
