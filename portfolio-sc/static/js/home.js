643a0d2b4dd1bdd200724a23d5c87c4730306970
sourabhsc <chauhans.sourabh@gmail.com>
2021-05-11 20:06:39 +0200

    fixed the terminal links

--- portfolio-sc/static/js/home.js
+++ portfolio-sc/static/js/home.js
@@ -5,9 +5,9 @@ $(function() {
       "sourabh.AI -h <br/>" + 
       "><span class='caret'>$</span> skills: Python, Machine learning, Flask,  computer vision<br/> ^100" +
       /*"><span class='caret'>$</span> job: android auto at <a href='http://www.google.com/'>Google Munich</a><br/> ^100" +*/
-      "><span class='caret'>$</span> hobbies: Badminton, Travel, <a href='http://www.rish.space/blog'>writing</a><br/> ^300" +
+      "><span class='caret'>$</span> hobbies: Badminton, Travel, gardening<br/> ^300" +
       "><span class='caret'>$</span> alias: sourabhsc <br/>" +
-      "><span class='caret'>$</span> highlight:  <a href='/projects/NULIRG'>Image segmentation on HST ULIRGs</a>, <a href='/projects/dharavicovid'>A JHU covid challenge solution</a><br/>"/*
+      "><span class='caret'>$</span> highlight:  <a href='https://nulirg.herokuapp.com/index.html'>Image segmentation on HST ULIRGs</a>, <a href='http://dharavicovid.herokuapp.com/'>A JHU covid challenge solution</a><br/>"/*
       "><span class='caret'>$</span> <a href='/timeline'>timeline</a> <a href='http://www.github.com/crearo/'>github</a> <a href='http://in.linkedin.com/in/bhardwajrish/'>linkedin</a> <a href='http://bhardwajrish.blogspot.com/'>blog</a><br/>"*/
     ],
     showCursor: true,
