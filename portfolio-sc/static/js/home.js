3aaa000bbc1132caf8fe466af067950f553aa0ac
Sourabh Chauhan <chauhans.sourabh@gmail.com>
2025-02-02 01:06:26 +0100

    Update home.js
    Just edided the home page with new information with knowledge graph etc.

--- portfolio-sc/static/js/home.js
+++ portfolio-sc/static/js/home.js
@@ -2,13 +2,12 @@ $(function() {
   /* NOTE: hard-refresh the browser once you've updated this */
   $(".typed").typed({
     strings: [
-      "sourabh.AI -h <br/>" + 
-      "><span class='caret'>$</span> skills: Python, Machine learning, Flask,  computer vision<br/> ^100" +
+      "sourabhsc.ai -h <br/>" + 
+      "><span class='caret'>$</span> Skills: Machine learning: NLP, Computer Vision, Knowledge Graphs:SPARQL, RAG <br/> ^100" +
       /*"><span class='caret'>$</span> job: android auto at <a href='http://www.google.com/'>Google Munich</a><br/> ^100" +*/
-      "><span class='caret'>$</span> hobbies: Badminton, Travel, gardening<br/> ^300" +
-      "><span class='caret'>$</span> alias: sourabhsc <br/>" +
-      "><span class='caret'>$</span> highlight:  <a href='https://nulirg.herokuapp.com/index.html'>Image segmentation on HST ULIRGs</a>, <a href='http://dharavicovid.herokuapp.com/'>A JHU covid challenge solution</a><br/>"/*
-      "><span class='caret'>$</span> <a href='/timeline'>timeline</a> <a href='http://www.github.com/crearo/'>github</a> <a href='http://in.linkedin.com/in/bhardwajrish/'>linkedin</a> <a href='http://bhardwajrish.blogspot.com/'>blog</a><br/>"*/
+      "><span class='caret'>$</span> Hobbies: Badminton, Travel, gardening<br/> ^300" +
+      "><span class='caret'>$</span> Alias: sourabhsc <br/>" +
+      "><span class='caret'>$</span> Highlights:  <a href='https://sourabhsc.github.io/NULIRG'>Image segmentation on HST ULIRGs</a>, <a href='http://dharavicovid.onrender.com/'>A JHU covid challenge solution</a><br/>"
     ],
     showCursor: true,
     cursorChar: '_',
